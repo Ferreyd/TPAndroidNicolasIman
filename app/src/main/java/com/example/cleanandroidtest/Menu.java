a33ea1b334262ac86da7c9d69f2bbb680f993917
Nicolas Cordina <cordina.nicolas@gmail.com>
2014-10-19 22:59:34 +0200

    Ajout redirection menu

--- app/src/main/java/com/example/cleanandroidtest/Menu.java
+++ app/src/main/java/com/example/cleanandroidtest/Menu.java
@@ -1,6 +1,7 @@
 package com.example.cleanandroidtest;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,26 +12,37 @@ import android.widget.Toast;
 
 public class Menu extends Activity {
 
-	 protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.menu);
-	        
-	        GridView gridView= (GridView) findViewById(R.id.gridview);
-	        gridView.setAdapter(new ImageAdapter(this));
-	        gridView.setOnItemClickListener(new OnItemClickListener() {
-	    
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-					// TODO Auto-generated method stub
-					//System.out.println("Welcome Iman !!!");
-		        	//Toast.makeText(Menu.this, ""+position, Toast.LENGTH_SHORT).show();
-					Toast.makeText(Menu.this, "Welcome Iman !!!", Toast.LENGTH_SHORT).show();
-					
-						Intent intent = new Intent (Menu.this, MonCompte.class);
-			            startActivity(intent);
-			        }
-			});
-	        
-	 }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        GridView gridView= (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                //System.out.println("Welcome Iman !!!");
+                //Toast.makeText(Menu.this, ""+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(Menu.this, "TOAST => SELECTION", Toast.LENGTH_SHORT).show();
+
+                if(position == 0)
+                {
+                    Toast.makeText(Menu.this, "TOAST => VERS MON COMPTE", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent (Menu.this, MonCompte.class);
+                    startActivity(intent);
+                }
+                if(position == 1)
+                {
+                    Toast.makeText(Menu.this, "TOAST => VERS LA LISTE", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent (Menu.this, MonCompte.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
 }
